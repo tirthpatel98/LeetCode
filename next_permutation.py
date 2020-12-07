14192b7af80361b74b587f335b4e1da2f4260cb1
Tirth Patel <37892475+tirthpatel98@users.noreply.github.com>
2020-12-06 20:11:53 -0800

    Create next_permutation.py

---
+++ next_permutation.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        nums1 = nums[:]
+        i = len(nums)-2
+        flag=0
+        while i >= 0:
+            curr = nums[i]
+            maxi = 101
+            for j in range(i+1,len(nums)):
+                if nums[j] > nums[i] and nums[j] < maxi:
+                    maxi=nums[j]
+                    ind=j
+            if maxi!=101:
+                nums[i],nums[ind] = nums[ind],nums[i]
+                nums[i+1:] = sorted(nums[i+1:])
+                flag=1   
+            if flag == 0:
+                i-=1
+            else:break
+    
+        if nums == nums1:
+            nums[0:]=sorted(nums)
+            
+        
